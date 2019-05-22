26f01c14a9443f6ca08fdc7f38169cb45448cf3c
Travis Stevens <travis.stevens@gaia.com>
2019-05-22 00:00:48 -0400

    refactoring stuff

--- reveal-js/js/reveal.js
+++ reveal-js/js/reveal.js
@@ -2027,7 +2027,8 @@
 							slide.style.top = 0;
 						}
 						else {
-							slide.style.top = Math.max( ( size.height - slide.scrollHeight ) / 2, 0 ) + 'px';
+							slide.style.top = 0;
+							//slide.style.top = Math.max( ( size.height - slide.scrollHeight ) / 2, 0 ) + 'px';
 						}
 					}
 					else {
